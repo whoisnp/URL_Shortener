b9858a7b56c8a8b1c0c092b6c9b8507b53d5cb39
Nithya Prakash <52366852+whoisnp@users.noreply.github.com>
2020-03-18 07:47:49 +0530

    Add files via upload

---
+++ url_shortener.py
@@ -0,0 +1,9 @@
+import urllib.request
+
+def tiny_url(url):
+    apiurl = "http://tinyurl.com/api-create.php?url="
+    tinyurl = urllib.request.urlopen(apiurl + url).read()
+    return tinyurl.decode("utf-8")
+
+url = input('enter the url to short :')
+print(tiny_url(url))
